569652871899a6cfa929d65b50bbf7834c668a89
Shafiur Rahman Bhauyan <shafiurrahman1241@gmail.com>
2022-04-27 18:16:33 -0500

    This plugin will send the player to the sky by explosion of tnt

--- FlyguyMC/src/mcpc/PhilosophyWithJosh/FlyguyMC/commands/TntCommand.java
+++ FlyguyMC/src/mcpc/PhilosophyWithJosh/FlyguyMC/commands/TntCommand.java
@@ -1,8 +1,13 @@
 package mcpc.PhilosophyWithJosh.FlyguyMC.commands;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 import mcpc.PhilosophyWithJosh.FlyguyMC.Main;
 
@@ -16,8 +21,35 @@ public class TntCommand implements CommandExecutor
 		plugin.getCommand("flyguy").setExecutor(this);
 	}
 	
-	public boolean onCommand(CommandSender sender, Command beblockhunter, String label, String[] args)
+	public boolean onCommand(CommandSender sender, Command flyguy, String label, String[] args)
 	{
-		return true;
+		if (sender instanceof Player) {
+			Player p = (Player)sender;
+			Location playerLoc = p.getLocation().clone();
+			Location tntLoc = p.getLocation().clone().subtract(0,1,0);;
+			Location redLock = p.getLocation().clone().subtract(0,2,0);;
+			Location obsidianOne = playerLoc.subtract(1,0,0);
+			Location obsidianTwo = playerLoc.subtract(0,0,1);
+			Location obsidianThree = playerLoc.subtract(1,0,0);
+			Location obsidianFour = playerLoc.subtract(0,0,1);
+			
+			Block blockOne = obsidianOne.getBlock();
+			blockOne.setType(Material.OBSIDIAN);
+			Block blockTwo = obsidianOne.getBlock();
+			blockOne.setType(Material.OBSIDIAN);
+			Block blockThree = obsidianOne.getBlock();
+			blockOne.setType(Material.OBSIDIAN);
+			Block blockFour = obsidianOne.getBlock();
+			blockOne.setType(Material.OBSIDIAN);
+		
+			Block waterBlock = tntLoc.getBlock();
+			waterBlock.setType(Material.WATER);
+			
+			p.sendMessage("Hi it's Shafiur. It's fun");
+			return true;
+
+
+		}
+		return false;
 	}
 }
